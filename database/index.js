90c96a1157f8c886e43e5631cd0745f91dfeee66
David Jeong <davidjeong989@gmail.com>
2021-01-26 20:22:10 -0800

    jest running with a few api routes tested.

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/images');
+mongoose.connect('mongodb://localhost/images', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
---
+++ testing/server.test.js
@@ -0,0 +1,51 @@
+const {MongoClient} = require('mongodb');
+const request = require('supertest');
+const app = require('../server/index');
+
+// const app = express()
+
+describe('insert', () => {
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect('mongodb://localhost/images', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    db = await connection.db('jest');
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
+
+});
+
+describe('Sample Test', () => {
+  it('should test that true === true', () => {
+    expect(true).toBe(true)
+  })
+})
+
+
+describe('GET Endpoints', () => {
+  it('should GET main images', async (done) => {
+    const res = await request(app).get('/images/mainImages/4');
+      expect(200)
+      done();
+  })
+
+  it('should GET detail images', async (done) => {
+    const res = await request(app).get('/images/detailImages/14');
+      expect(200)
+      done();
+  })
+
+  it('should GET thumbnail images', async (done) => {
+    const res = await request(app).get('/images/thumbnailImages/42');
+      expect(200)
+      done();
+  })
+})
+
