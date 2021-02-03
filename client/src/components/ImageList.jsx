38463c4ab81a41bc283207af2e385bfb77122da2
David Jeong <davidjeong989@gmail.com>
2021-02-02 17:03:09 -0800

    final commit

--- client/src/components/ImageList.jsx
+++ client/src/components/ImageList.jsx
@@ -1,36 +1,78 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import Image from './Image.jsx'
 import styled from 'styled-components'
 import {LeftArrow, RightArrow, Pictures, Slider} from './styled.js'
 
 
 const ImageList = (props) => {
-const [pic, setPic] = useState(0)
+const [pic, setPic] = useState(-50)
 const length = props.data.length;
+const [slide, setSlide] = useState(true)
+const [prev, setPrev] = useState(0);
+
+// const usePrevious = (value) => {
+//   const ref = useRef();
+
+//   useEffect(() => {
+//     ref.current = value;
+//   });
+
+//   return ref.current;
+// }
+
+// const previous = usePrevious(pic);
+
+// useEffect(() => {
+//   console.log('useEFFECT', pic)
+
+//   if(pic === 0) {
+//     setSlide(false)
+//   } else {
+//     setSlide(true)
+//   }
+// });
 
 const nextSlide = () => {
-  console.log(pic)
-  pic === -100 * (props.data.length -1) ? setPic(0) : setPic(pic - 100)
+  pic === ((length * -100) -50)? setPic(-50) : setPic(pic => pic-100);
+
 }
 
 const prevSlide = () => {
-  console.log(pic)
-  pic === 0 ? setPic(-100 * (props.data.length-1)) : setPic(pic + 100)
+  pic === 50 ? setPic((-100 * (length)) -50) : setPic(pic => pic + 100)
+
+
 }
+
   return(
     <div className='slider'>
-      <LeftArrow id='prev' onClick={prevSlide}/>
-      <RightArrow id='next' onClick={nextSlide}/>
+
+      <LeftArrow onClick={prevSlide}>&lt;</LeftArrow>
+      <RightArrow id='next' onClick={nextSlide}>&gt;</RightArrow>
+
+      <div key={-1} className={pic === -50 ? null: 'slide'} style={{transform: `translateX(${pic}%)`}}>
+        <img src={props.data[length-1]} className='image'/>
+      </div>
+
       {props.data.map((image, id) => {
         return (
-          <div className='slide' key={id} style={{transform: `translateX(${pic}%)`}}>
-           <Image image={image} key={id}/>
+          <div className={pic === -50 ? null: 'slide'} key={id+100} style={{transform: `translateX(${pic}%)`}}>
+
+            <Image image={image} key={id+1}/>
+
           </div>
 
         )
       })}
+
+      <div key={props.data.length} className={pic === -50 ? null: 'slide'} style={{transform: `translateX(${pic}%)`}}>
+        <img src={props.data[0]} key={props.data.length+2}className='image'/>
+
+      </div>
+
     </div>
   );
 }
+
+
 
 export default ImageList;
