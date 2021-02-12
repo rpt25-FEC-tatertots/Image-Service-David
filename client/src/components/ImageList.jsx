ffeb21f9e49146053f7c1902cc3e18e8dd19181f
Danny Harmon <dannyaharmon@gmail.com>
2021-02-12 15:15:58 -0800

    Refactor component

--- client/src/components/ImageList.jsx
+++ client/src/components/ImageList.jsx
@@ -2,30 +2,71 @@ import React from 'react'
 import styled from 'styled-components'
 
 const ImageWrapper = styled.div`
-
+display: flex;
+justify-content: center;
+align-items: center;
 `;
 const ImageSlider = styled.div`
+width: 100%;
 display: flex;
-flex-wrap: none;
+justify-content: center;
+align-items: center;
+overflow: hidden;
 `;
 const Image = styled.img`
+width: 66%;
+@media (max-width: 767px) {
+  width: 100%
+}
 `;
-const Arrow = styled.div`
+const NextArrow = styled.div`
+position: absolute;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 2em;
+right: 4rem;
+width: 50px;
+height: 50px;
 border-radius: 50%;
+font-style: bold;
 color: black;
 background-color: white;
+@media (max-width: 767px) {
+  display: none;
+}
+`;
+const PrevArrow = styled.div`
+position: absolute;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 2em;
+left: 4rem;
+width: 50px;
+height: 50px;
+border-radius: 50%;
+font-style: bold;
+color: black;
+background-color: white;
+@media (max-width: 767px) {
+  display: none;
+}
 `;
 
-const ImageList = ({data, active}) => {
+const ImageList = ({data, active, slideNext, slidePrev}) => {
+
+  const prev = active === 0 ? data.length - 1 : active - 1;
+  const next = active === data.length - 1 ? 0 : active + 1;
   return (
     <ImageWrapper>
-      <Arrow>&#60</Arrow>
+      <PrevArrow onClick={() => slidePrev()}>&lt;</PrevArrow>
       <ImageSlider>
-        {data.map((url, i) => {
-          return (<Image src={url} alt="Image" />)
-        })}
+        <Image src={data[prev]} alt="image" />
+        <Image src={data[active]} alt="image" />
+        <Image src={data[next]} alt="image" />
       </ImageSlider>
-      <Arrow>&#60</Arrow>
+      <NextArrow onClick={() => slideNext()}>&gt;</NextArrow>
     </ImageWrapper>
   )
 }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,11 +6,33 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      mainImages: []
+      mainImages: [],
+      activeImageIndex: 0
     }
+    this.slideNext = this.slideNext.bind(this);
+    this.slidePrev = this.slidePrev.bind(this);
   }
-
+slideNext() {
+  const { activeImageIndex, mainImages } = this.state;
+  if (activeImageIndex === mainImages.length - 1) {
+    this.setState({ activeImageIndex: 0 });
+  } else {
+    this.setState({ activeImageIndex: activeImageIndex + 1 })
+  }
+}
+slidePrev() {
+  const { activeImageIndex, mainImages } = this.state;
+  if (activeImageIndex === 0) {
+    this.setState({ activeImageIndex: mainImages.length - 1 });
+  } else {
+    this.setState({ activeImageIndex: activeImageIndex - 1 });
+  }
+}
 componentDidMount() {
+  window.addEventListener('hashchange', () => {
+    let activeImageIndex = window.location.hash.split("&")[1].split("=")[1]
+    this.setState({ activeImageIndex })
+  })
   axios.get(`/images/mainImages${window.location.pathname}`)
     .then(response => {
       this.setState({mainImages: response.data.mainImages})
@@ -19,12 +41,20 @@ componentDidMount() {
       console.log(error)
     })
 }
+componentWillUnmount() {
+  window.removeEventListener('hashchange')
+}
 
 
 render() {
   return (
     <div>
-      <ImageList data={this.state.mainImages}/>
+      <ImageList
+      data={this.state.mainImages}
+      active={this.state.activeImageIndex}
+      slidePrev={this.slidePrev}
+      slideNext={this.slideNext}
+      />
     </div>
     )
 }
