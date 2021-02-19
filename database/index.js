d0c2134a6a78f1274a292d5e47e028f678171f6e
Danny Harmon <dannyaharmon@gmail.com>
2021-02-18 19:49:41 -0800

    use changed variable to connect

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const mongoURI = process.env.MONGO_URI || 'mongodb://localhost/FEC';
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
